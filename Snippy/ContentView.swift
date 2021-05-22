feeba5aa23867b9b38cba4100ed13f079bafc3f7
Jorge Benavides <bogue89@gmail.com>
2021-05-22 01:31:47 -0500

    Initial Commit

---
+++ Snippy/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Snippy
+//
+//  Created by Jorge Alberto Benavides Ojinaga on 22/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
